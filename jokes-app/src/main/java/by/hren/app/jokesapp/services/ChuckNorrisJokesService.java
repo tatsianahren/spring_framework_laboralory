6340b71aabbe522e246751e970c09595a210c0f9
tatsiana.hren <grentatsiana@gmail.com>
2018-05-10 20:08:19 +0300

    Java base configuration

--- jokes-app/src/main/java/by/hren/app/jokesapp/services/ChuckNorrisJokesService.java
+++ jokes-app/src/main/java/by/hren/app/jokesapp/services/ChuckNorrisJokesService.java
@@ -8,8 +8,8 @@ public class ChuckNorrisJokesService implements JokesService {
 
     private final ChuckNorrisQuotes chuckNorrisQuotes;
 
-    public ChuckNorrisJokesService() {
-        this.chuckNorrisQuotes = new ChuckNorrisQuotes();
+    public ChuckNorrisJokesService(ChuckNorrisQuotes chuckNorrisQuotes) {
+        this.chuckNorrisQuotes = chuckNorrisQuotes;
     }
 
     @Override
