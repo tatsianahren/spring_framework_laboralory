1f821307bd48708ed351eba0abe0e1a97175d19a
tatsiana.hren <grentatsiana@gmail.com>
2018-05-12 22:03:42 +0300

    Spring boot properties properties configuration

--- di-demo/src/main/java/com/hren/didemo/config/PropertiesConfig.java
+++ di-demo/src/main/java/com/hren/didemo/config/PropertiesConfig.java
@@ -5,11 +5,9 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
 @Configuration
-@PropertySource("classpath:datasource.properties")
 public class PropertiesConfig {
 
     @Value("${app.username}")
