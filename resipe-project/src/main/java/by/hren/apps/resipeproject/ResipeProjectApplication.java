12f982193c5bea18333cb9d728010e7d48799346
tatsiana.hren <grentatsiana@gmail.com>
2018-05-12 23:16:50 +0300

    Added new project

---
+++ resipe-project/src/main/java/by/hren/apps/resipeproject/ResipeProjectApplication.java
@@ -0,0 +1,12 @@
+package by.hren.apps.resipeproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResipeProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResipeProjectApplication.class, args);
+    }
+}
---
+++ resipe-project/src/test/java/by/hren/apps/resipeproject/ResipeProjectApplicationTests.java
@@ -0,0 +1,16 @@
+package by.hren.apps.resipeproject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ResipeProjectApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
