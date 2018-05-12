546ed4edd4bf87645d06574b44f704d44d43e46c
tatsiana.hren <grentatsiana@gmail.com>
2018-05-12 23:25:41 +0300

    Added added index page

--- resipe-project/src/test/java/by/hren/apps/resipeproject/RecipeProjectApplicationTests.java
+++ resipe-project/src/test/java/by/hren/apps/resipeproject/RecipeProjectApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class ResipeProjectApplicationTests {
+public class RecipeProjectApplicationTests {
 
     @Test
     public void contextLoads() {
