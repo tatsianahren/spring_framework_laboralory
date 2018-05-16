9794c1f3a27053bbb515dbf74c8c9ded5a4ed7be
tatsiana.hren <grentatsiana@gmail.com>
2018-05-16 21:25:37 +0300

    Added new pojo's

---
+++ resipe-project/src/main/java/by/hren/apps/resipeproject/domain/Notes.java
@@ -0,0 +1,40 @@
+package by.hren.apps.resipeproject.domain;
+
+import javax.persistence.*;
+
+@Entity
+public class Notes {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    private Recipe recipe;
+    @Lob
+    private String text;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Recipe getRecipe() {
+        return recipe;
+    }
+
+    public void setRecipe(Recipe recipe) {
+        this.recipe = recipe;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
