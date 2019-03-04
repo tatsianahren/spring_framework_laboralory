61afec4f5d9565c0a97134ea837192967aa3d252
Tatsiana Hren <t.hren@mps-solutions.de>
2019-03-04 15:02:52 +0300

    refactoring

--- di-demo/src/main/java/com/hren/didemo/services/PrimarySpanishGreetingService.java
+++ di-demo/src/main/java/com/hren/didemo/services/PrimarySpanishGreetingService.java
@@ -1,9 +1,5 @@
 package com.hren.didemo.services;
 
-import org.springframework.context.annotation.Primary;
-import org.springframework.context.annotation.Profile;
-import org.springframework.stereotype.Service;
-
 public class PrimarySpanishGreetingService implements GreetingService {
     private GreetingRepository greetingRepository;
 
